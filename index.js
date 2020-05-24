8f04a384d6d8b64907383fe69751f549233e64f6
Iain Bean <iainmbean@gmail.com>
2020-05-24 12:47:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+fs = require('fs');
+const dotenv = require('dotenv');
+const Twit = require('twit');
+
+dotenv.config();
+
+const T = new Twit({
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+  strictSSL: true, // optional - requires SSL certificates to be valid.
+});
+
+T.get(
+  '/users/show',
+  { screen_name: process.env.TWITTER_SCREEN_NAME },
+  (err, data, response) => {
+    let tweets = [];
+    let remainingTweets = data.statuses_count;
+    let maxId = null;
+
+    (function getTweets() {
+      if (remainingTweets > 0) {
+        T.get(
+          'statuses/user_timeline',
+          {
+            screen_name: process.env.TWITTER_SCREEN_NAME,
+            count: 200,
+            tweet_mode: 'extended',
+            ...(maxId ? { max_id: maxId } : {}),
+          },
+          (err, data, response) => {
+            remainingTweets = remainingTweets - data.length;
+            maxId = data[data.length - 1].id;
+            tweets = [...tweets, ...data.map(({ full_text }) => full_text)];
+            getTweets();
+          }
+        );
+      } else {
+        fs.writeFileSync('tweets.json', JSON.stringify(tweets));
+      }
+    })();
+  }
+);
